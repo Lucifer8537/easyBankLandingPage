7a842e3d49a1a02350a24e020c53f1e74628ff24
navodit <navodit50@gmail.com>
2024-03-19 20:43:30 +0530

    desktop changes

--- src/app/mobile-bg/mobile-bg.component.ts
+++ src/app/mobile-bg/mobile-bg.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+interface feature {
+  imgSrc: string;
+  title: string;
+  context: string;
+}
 @Component({
   selector: 'app-mobile-bg',
   templateUrl: './mobile-bg.component.html',
@@ -10,4 +14,30 @@ export class MobileBgComponent {
   context =
     'Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.';
   btnLabel = 'Request Invite';
+  featureList: feature[] = [
+    {
+      imgSrc: '../../assets/icon-online.svg',
+      title: 'Online Banking',
+      context:
+        'Our modern web and mobile application allow you to keep track of your finances wherever you are in the world.',
+    },
+    {
+      imgSrc: '../../assets/icon-budgeting.svg',
+      title: 'Simple Budgeting',
+      context:
+        "See exactly where your money goes each month. Receive notifications when you're close to hitting your limits.",
+    },
+    {
+      imgSrc: '../../assets/icon-onboarding.svg',
+      title: 'Fast Onboarding',
+      context:
+        "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
+    },
+    {
+      imgSrc: '../../assets/icon-api.svg',
+      title: 'OpenAPI',
+      context:
+        'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
+    },
+  ];
 }
